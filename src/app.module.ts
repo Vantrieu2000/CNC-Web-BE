c7c73b22864d3d19b4ca9a9facc8a757aa141461
Vantrieu <trieunguyen200064@gmail.com>
2023-09-15 17:35:22 +0700

    remove jwtservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,16 @@
-import { JwtService } from '@nestjs/jwt';
-import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { CommonModule } from './common/common.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { AuthorizationGuard } from './modules/auth/authorization.guard';
-import { UploadModule } from './modules/upload/upload.module';
-import { ProductsModule } from './modules/products/products.module';
-import { ImagesModule } from './modules/images/images.module';
-import { CategoryModule } from './modules/category/category.module';
-import { DetailProductModule } from './modules/detail-product/detail-product.module';
+import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+import { CommonModule } from "./common/common.module";
+import { AuthModule } from "./modules/auth/auth.module";
+import { AuthorizationGuard } from "./modules/auth/authorization.guard";
+import { CategoryModule } from "./modules/category/category.module";
+import { DetailProductModule } from "./modules/detail-product/detail-product.module";
+import { ImagesModule } from "./modules/images/images.module";
+import { ProductsModule } from "./modules/products/products.module";
+import { UploadModule } from "./modules/upload/upload.module";
 
 @Module({
-  imports: [
-    CommonModule,
-    AuthModule,
-    UploadModule,
-    JwtService,
-    ProductsModule,
-    ImagesModule,
-    CategoryModule,
-    DetailProductModule,
-  ],
+  imports: [CommonModule, AuthModule, UploadModule, ProductsModule, ImagesModule, CategoryModule, DetailProductModule],
   controllers: [],
   providers: [
     {
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,24 +1,22 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
 // import { JwtModule } from '@nestjs/jwt';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { WinstonModuleFor } from 'src/winstonModule';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { WinstonModuleFor } from "src/winstonModule";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forFeature([
-
-    ]),
+    TypeOrmModule.forFeature([]),
     // JwtModule.register({
     //   secret: process.env.JWT_SECRET_KEY,
     //   signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
     // }),
-    WinstonModuleFor('auth'),
+    WinstonModuleFor("auth"),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, JwtService],
